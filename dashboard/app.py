0bd4434ac827c5429252a10a369c31925b7358fa
Matt Gevaux <matt.gevaux@hotmail.com>
2025-12-22 09:33:14 +0000

    Updated module imports

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import threading
 import time
 from flask import Flask, render_template, jsonify
