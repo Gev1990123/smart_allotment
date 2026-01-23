0a66092ba82f3bf78b35bfaae4d8cf622c815299
Matt Gevaux <matt.gevaux@hotmail.com>
2026-01-23 10:28:17 +0000

    Updated config.py - to reflect correct DB credentials.

--- hub/api/config.py
+++ hub/api/config.py
@@ -4,7 +4,7 @@ class Settings:
     PSQL_HOST = os.getenv("PSQL_HOST", "database")
     PSQL_PORT = int(os.getenv("PSQL_PORT", 5432))
     PSQL_USER = os.getenv("PSQL_USER", "mqtt")
-    PSQL_PASS = os.getenv("PSQL_PASS", "mqtt123")
+    PSQL_PASS = os.getenv("PSQL_PASS", "smartallotment2026")
     PSQL_DB   = os.getenv("PSQL_DB", "sensors")
 
 settings = Settings()
